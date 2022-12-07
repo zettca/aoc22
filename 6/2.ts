322fc985204f8d1483a4f06ce9642c4bd68f6aa8
Bruno <zettca@gmail.com>
2022-12-07 10:16:38 +0000

    add day 6

---
+++ 6/2.ts
@@ -0,0 +1,14 @@
+const file = Deno.readTextFileSync("./input.txt").trimEnd();
+
+const getFirstUniqueIndex = (str: string, num: number) => {
+  const regexRepeating = /^.*(.).*\1.*$/;
+
+  for (let i = 0; i < str.length; i++) {
+    const group = str.slice(i, i + num);
+    if (!regexRepeating.test(group)) return i + num;
+  }
+
+  return -1;
+};
+
+console.log(getFirstUniqueIndex(file, 14));
