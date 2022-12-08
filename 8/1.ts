559225a6af7f08abcc5aada30ea00a20d6200f82
Bruno <zettca@gmail.com>
2022-12-08 16:28:39 +0000

    refactor to chad 😎 array iterators (reducer)

--- 8/1.ts
+++ 8/1.ts
@@ -2,30 +2,19 @@ const file = Deno.readTextFileSync("./input.txt").trimEnd();
 
 const lines = file.split("\n").map((l) => l.split("").map(Number));
 
-const prettyLines = lines.map(() => [] as string[]);
+const getMax = (arr: number[]) => (arr.length ? Math.max(...arr) : -1);
+const getColumn = <T>(arr: T[][], i: number) => arr.map((l) => l[i]);
+const splitAt = <T>(arr: T[], i: number) => [arr.slice(0, i), arr.slice(i + 1)];
 
-const getColumn = <T>(arr: T[][], idx: number) => arr.map((l) => l[idx]);
+const numVisible = lines.reduce((acc, line, x) => {
+  const numVisibleLine = line.reduce((acc2, value, y) => {
+    const column = getColumn(lines, y);
+    const maxes = [...splitAt(line, y), ...splitAt(column, x)].map(getMax);
 
-let numVisible = 0;
+    return acc2 + Number(value > Math.min(...maxes)); // dirty hack or nice? 🤔
+  }, 0);
 
-for (let l = 0; l < lines.length; l++) {
-  const line = lines[l];
-  for (let c = 0; c < line.length; c++) {
-    const tree = line[c];
-    const column = getColumn(lines, c);
-
-    const maxes = [
-      column.slice(0, l),
-      column.slice(l + 1),
-      line.slice(0, c),
-      line.slice(c + 1),
-    ].map((arr) => (arr.length > 0 ? Math.max(...arr) : -1));
-
-    const isTallest = maxes.some((max) => tree > max);
-    if (isTallest) numVisible++;
-
-    prettyLines[l][c] = isTallest ? String(tree) : " ";
-  }
-}
+  return acc + numVisibleLine;
+}, 0);
 
 console.log(numVisible);
